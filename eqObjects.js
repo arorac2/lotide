ca1a2cb4ccb89414727704a171e593119f7e77b8
Chaahat <arorac2@mymacewan.ca>
2023-05-03 16:24:00 -0600

    implemented eqObjects function

---
+++ eqObjects.js
@@ -0,0 +1,58 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const eqArrays = function(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+      return false;
+    }
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  };
+  
+  const eqObjects = function(object1, object2) {
+    const object1Keys = Object.keys(object1);
+    const object2Keys = Object.keys(object2);
+  
+    if (object1Keys.length !== object2Keys.length) {
+      return false;
+    }
+  
+    for (const key of object1Keys) {
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        if (!eqArrays(object1[key], object2[key])) {
+          return false;
+        }
+      } else if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+  
+    return true;
+  };
+  
+  // Tests for primitive values
+  const shirtObject = { color: "red", size: "medium" };
+  const anotherShirtObject = { size: "medium", color: "red" };
+  assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
+  
+  const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+  assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
+  
+  // Tests for array values
+  const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+  const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+  assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true);
+  
+  const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+  assertEqual(eqObjects (multiColorShirtObject, longSleeveMultiColorShirtObject), false);
+  console.log("multi is", multiColorShirtObject);
