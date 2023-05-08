96230fc97f62267752670d2db686344eccad546f
Chaahat <arorac2@mymacewan.ca>
2023-05-08 17:09:16 -0600

    added test cases for eqArrays.js

---
+++ test/eqArraysTest.js
@@ -0,0 +1,8 @@
+const eqArrays = require('../eqArrays');
+const assertEqual = require('../assertEqual');
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // should pass
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // should pass
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // should pass
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // should pass
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), true); // => should fail
