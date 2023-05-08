b3b8ab97b43e666386c0f1ed487cd855f9804f31
Chaahat <arorac2@mymacewan.ca>
2023-05-08 17:42:27 -0600

    added test to use chai for middle.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,29 @@
-const middle = require("../middle");
+const assert = require('chai').assert;
+const middle   = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual');
-// TEST CODE
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+
+describe("#middle", () => {
+    it("returns an empty array for [1]", () => {
+      assertArraysEqual(middle([1]), []);
+    });
+  
+    it("returns an empty array for [1, 2]", () => {
+      assertArraysEqual(middle([1, 2]), []);
+    });
+  
+    it("returns [2] for [1, 2, 3]", () => {
+      assertArraysEqual(middle([1, 2, 3]), [2]);
+    });
+  
+    it("returns [3] for [1, 2, 3, 4, 5]", () => {
+      assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+    });
+  
+    it("returns [2, 3] for [1, 2, 3, 4]", () => {
+      assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+    });
+  
+    it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+      assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+    });
+  });
